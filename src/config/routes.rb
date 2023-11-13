5e042001f0b43a415f9bff1ebbc16bfee9d3297d
Tushar Banik <b.toshar@iitg.ac.in>
2023-11-13 09:08:12 +0530

    login and signup routes

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "signup" => "signup#index", as: :signup
+
+  get "login" => "login#index", as: :login
+
   get "blog/:id" => "blog_posts#show", as: :blog_post
 
   # Defines the root path route ("/")
