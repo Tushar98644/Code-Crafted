e8e01cf35ef4a3b50e13af2766aebafdf538f2f8
Tushar Banik <b.toshar@iitg.ac.in>
2023-11-13 12:27:37 +0530

    authentication using devise

--- src/app/controllers/blog_posts_controller.rb
+++ src/app/controllers/blog_posts_controller.rb
@@ -1,9 +1,17 @@
 class BlogPostsController < ApplicationController
     def index
+      if user_signed_in?
+      else
+        redirect_to new_user_session_path
+      end
       @blog_posts = BlogPost.all
     end
 
     def show 
+      if user_signed_in?
+      else
+        redirect_to new_user_session_path
+      end
       @blog_post = BlogPost.find(params[:id])
     end
 end
