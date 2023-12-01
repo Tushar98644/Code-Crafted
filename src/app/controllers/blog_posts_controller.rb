d6292762d4b01f75628fa11884f18de57bdb7122
Tushar Banik <b.toshar@iitg.ac.in>
2023-12-01 12:03:55 +0530

    display image

--- src/app/controllers/blog_posts_controller.rb
+++ src/app/controllers/blog_posts_controller.rb
@@ -1,38 +1,38 @@
+# frozen_string_literal: true
+
 class BlogPostsController < ApplicationController
-  
-   before_action :authenticate_user!
+  before_action :authenticate_user!
   #  authenticate_user! is a Devise helper method that ensures that the user is logged in before they can access the controller actions.
 
-    def index
-        @all_blog_posts = BlogPost.all
-    end
+  def index
+    @all_blog_posts = BlogPost.all
+  end
 
-    def show 
-        @blog_post = BlogPost.find(params[:id])
-    end
+  def show
+    @blog_post = BlogPost.find(params[:id])
+  end
 
-    def new
-    end
+  def new; end
 
-    def create
-      @blog_post = current_user.blog_posts.build(blog_post_params)
-  
-      if @blog_post.save
-        # Successfully created a blog post
-        redirect_to root_path, notice: 'Blog post was successfully created.'
-      else
-        blog_post.errors.full_messages
-        # Failed to create a blog post, render the form again with errors
-        render :new
-        # => ["Title can't be blank", "Description can't be blank", "Body can't be blank"]
-      end
-    end
+  def create
+    @blog_post = current_user.blog_posts.build(blog_post_params)
 
-    def blog_post_params
-      params.permit(:title, :description, :body)
+    if @blog_post.save
+      # Successfully created a blog post
+      redirect_to root_path, notice: 'Blog post was successfully created.'
+    else
+      blog_post.errors.full_messages
+      # Failed to create a blog post, render the form again with errors
+      render :new
+      # => ["Title can't be blank", "Description can't be blank", "Body can't be blank"]
     end
+  end
 
-    def user_blogs
-       @user_blog_posts = current_user.blog_posts
-    end
+  def blog_post_params
+    params.permit(:title, :description, :body, :image)
+  end
+
+  def user_blogs
+    @user_blog_posts = current_user.blog_posts
+  end
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  get "blog/:id" => "blog_posts#show", as: :blog_post
+  get 'blog/:id' => 'blog_posts#show', as: :blog_post
 
-  get "user_blogs" => "blog_posts#user_blogs", as: :my_blogs
-  get "new_blog" => "blog_posts#new", as: :new_blog
-  post "create" => "blog_posts#create", as: :create_blog
+  get 'user_blogs' => 'blog_posts#user_blogs', as: :my_blogs
+  get 'new_blog' => 'blog_posts#new', as: :new_blog
+  post 'create' => 'blog_posts#create', as: :create_blog
 
   # Defines the root path route ("/")
-  root "blog_posts#index"
+  root 'blog_posts#index'
 end
