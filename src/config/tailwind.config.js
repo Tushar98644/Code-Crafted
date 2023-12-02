081ad2cf019d6eb18a0e1a527d2136c7f5ac8b38
Tushar Banik <b.toshar@iitg.ac.in>
2023-12-02 10:41:36 +0530

    homepage carousel animations

--- src/config/tailwind.config.js
+++ src/config/tailwind.config.js
@@ -13,9 +13,23 @@ module.exports = {
       colors: {
         primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
       },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+        'reverse-infinite-scroll': 'reverse-infinite-scroll 25s linear infinite'
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
+        'reverse-infinite-scroll':{
+          from: { transform: 'translateX(-100%)' },
+          to: { transform: 'translateX(0%)' },
+        }
+      }  
     },
   },
   plugins: [
